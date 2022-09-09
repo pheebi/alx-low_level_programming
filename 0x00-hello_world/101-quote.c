74335161e1f98123e37e2b625ba94390a388a272
Phoebe Nkanta <phibink28@gmail.com>
2022-09-08 19:07:41 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-Task 8
-
 #include<unistd.h>
 /**
  * main - prints the standard error sandwiched around the  standard output
@@ -11,4 +9,4 @@ int main(void)
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
-} 
+}
