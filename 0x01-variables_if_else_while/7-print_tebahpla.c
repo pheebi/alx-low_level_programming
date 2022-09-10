b2347188cd51ecf875454fed048ae96c05dbb446
Phoebe Nkanta <phibink28@gmail.com>
2022-09-09 21:40:27 -0700

    Task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
-
 
 /**
  *
@@ -14,26 +10,16 @@
  *
  *    * Return: Always 0 (Success)
  *
- *     */
-
-
+*/
 
 int main(void)
-
 {
+	char low;
 
-		char low;
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
 
+	putchar('\n');
 
-
-			for (low = 'z'; low >= 'a'; low--)
-
-						putchar(low);
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	return (0);
 }
