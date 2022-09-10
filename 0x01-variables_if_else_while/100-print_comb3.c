fcdb17a7cc98559c1dace343ae7c04deeb331579
Phoebe Nkanta <phibink28@gmail.com>
2022-09-09 22:41:41 -0700

    Task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,69 +1,41 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ *  main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ *  Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
+	int d, p;
 
-		int d, p;
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
 
+			if (p != d)
+			{
 
+			putchar(d);
 
-			for (d = '0'; d < '9'; d++)
+			putchar(p);
 
-					{
+			if (d == '8' && p == '9')
+			continue;
 
-								for (p = d + 1; p <= '9'; p++)
+			putchar(',');
 
-											{
+			putchar(' ');
 
-															if (p != d)
+			}
+		}
+	}
 
-																			{
+	putchar('\n');
 
-																								putchar(d);
-
-																												putchar(p);
-
-
-
-																																if (d == '8' && p == '9')
-
-																																						continue;
-
-
-
-																																				putchar(',');
-
-																																								putchar(' ');
-
-																																											}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,77 +1,48 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ *  main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *  Return: Always 0 (Success)
+*/
 
 
 
 int main(void)
-
 {
+	int d, p, q;
 
-		int d, p, q;
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
 
+			for (q = p + 1; q <= '9'; q++)
+			{
 
+				if ((p != d) != q)
+				{
 
-			for (d = '0'; d < '9'; d++)
+					putchar(d);
 
-					{
+					putchar(p);
 
-								for (p = d + 1; p <= '9'; p++)
+					putchar(q);
 
-											{
+					if (d == '7' && p == '8')
+					continue;
 
-															for (q = p + 1; q <= '9'; q++)
+					putchar(',');
 
-																			{
+					putchar('');
+				}
+			}
+		}
+	}
 
-																								if ((p != d) != q)
+	putchar('\n');
 
-																													{
-
-																																			putchar(d);
-
-																																								putchar(p);
-
-																																													putchar(q);
-
-
-
-																																																		if (d == '7' && p == '8')
-
-																																																									continue;
-
-
-
-																																																							putchar(',');
-
-																																																												putchar(' ');
-
-																																																																}
-
-																											}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
