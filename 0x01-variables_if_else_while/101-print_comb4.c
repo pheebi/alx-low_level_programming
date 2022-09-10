4ceed80feb033c5868fda1972cb3d7700062f392
Phoebe Nkanta <phibink28@gmail.com>
2022-09-09 22:55:46 -0700

    Task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,12 +14,12 @@ int main(void)
 {
 	int d, p, q;
 
-	for (d = 0; d < 9; d++)
+	for (d = 0; d < 8; d++)
 	{
-		for (p = d + 1; p <= 9; p++)
+		for (p = d + 1; p < 9; p++)
 		{
 
-			for (q = p + 1; q <= 9; q++)
+			for (q = p + 1; q < 10; q++)
 			{
 
 				if ((p != d) != q)
